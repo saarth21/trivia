f0fe18297194aa20612e057b153b3dc90fe177f1
saarth21 <joshisaarth@gmail.com>
2025-03-03 17:52:24 -0500

    Finished trivia game
    I made the score and made sure it showed the points at the end based on the number of questions you get right or wrong.

--- main.py
+++ main.py
@@ -17,10 +17,13 @@ score = 0
 # Funtions that handles button press
 def buttonController(anwser):
 	# Work on next time - SCORING
+	global score
 	global question_index
+	if choices[question_index][anwser] == anwsers[question_index]:
+		score += 1
 	question_index +=1
 	if question_index == 4:
-		messagebox.showinfo("Quiz is complete","Your Score:")
+		messagebox.showinfo("Quiz is complete","Your Score: " + str(score))
 
 	#Config - change properties of a component
 	question_label.config(text=questions[question_index])
@@ -35,16 +38,16 @@ def buttonController(anwser):
 question_label = tk.Label(window, text=questions[0])
 question_label.pack(pady=10)
 #Making buttons
-button_label1 = tk.Button(window, text=choices[0][0], command=buttonController)
+button_label1 = tk.Button(window, text=choices[0][0], command=lambda:buttonController(0))
 button_label1.pack(pady=6)
 
-button_label2 = tk.Button(window, text=choices[0][1], command=buttonController)
+button_label2 = tk.Button(window, text=choices[0][1], command=lambda:buttonController(1))
 button_label2.pack(pady=6)
 
-button_label3 = tk.Button(window, text=choices[0][2], command=buttonController)
+button_label3 = tk.Button(window, text=choices[0][2], command=lambda:buttonController(2))
 button_label3.pack(pady=6)
 
-button_label4 = tk.Button(window, text=choices[0][3], command=buttonController)
+button_label4 = tk.Button(window, text=choices[0][3], command=lambda:buttonController(3))
 button_label4.pack(pady=6)
 
 
